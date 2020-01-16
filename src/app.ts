48d7a8720f0d68e3f45267f46e5c719222d891f6
UkYi <ukyi.js@gmail.com>
2020-01-17 00:30:23 +0900

    add app.ts

---
+++ src/app.ts
@@ -0,0 +1,58 @@
+import express, { Application } from 'express';
+import path from 'path';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import Controller from './interfaces/controller';
+import errorMiddleware from './middleware/error';
+import { INFO } from './utils/log';
+
+class App {
+  public app: Application;
+
+  public static of(controllers: Controller[]) {
+    return new App(controllers);
+  }
+
+  constructor(controllers: Controller[]) {
+    this.app = express();
+
+    this.listen();
+    this.initializeMiddlewares();
+    this.initializeControllers(controllers);
+    this.initializeErrorHandling();
+    // this.connectToTheDatabase();
+  }
+
+  public getServer() {
+    return this.app;
+  }
+
+  public listen() {
+    this.app.listen(process.env.PORT, () => INFO(`Server listening on http://localhost:${process.env.PORT}`));
+  }
+
+  private initializeMiddlewares() {
+    this.app.engine('html', require('ejs').renderFile);
+    this.app.set('view engine', 'html');
+    this.app.set('views', path.join(__dirname, 'views'));
+
+    this.app.use(cors());
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+  }
+
+  private initializeErrorHandling() {
+    this.app.use(errorMiddleware);
+  }
+
+  private initializeControllers(controllers: Controller[]) {
+    controllers.forEach(controller => this.app.use('/', controller.router));
+  }
+
+  // private connectToTheDatabase() {
+  //   const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH } = process.env;
+  //   mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
+  // }
+}
+
+export default App;
