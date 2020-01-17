37b021329ddeb26a4d2b8af2f9c68b08a5b19f5f
ukyiJS <ukyi.js@gmail.com>
2020-01-17 16:33:52 +0900

    add mongo env

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -3,9 +3,9 @@ import { cleanEnv, port, str } from 'envalid';
 export default function validateEnv() {
   cleanEnv(process.env, {
     PORT: port(),
-    JWT_SECRET: str()
-    // MONGO_PASSWORD: str(),
-    // MONGO_PATH: str(),
-    // MONGO_USER: str(),
+    JWT_SECRET: str(),
+    MONGO_PASSWORD: str(),
+    MONGO_PATH: str(),
+    MONGO_USER: str()
   });
 }
