8e627a23dba0467935a7e64a7da6a09b6953615c
UkYi <ukyi.js@gmail.com>
2020-02-15 20:54:28 +0900

    add color interface

---
+++ src/interfaces/color.ts
@@ -0,0 +1,11 @@
+interface Color {
+  white: 'white';
+  cyan: 'cyan';
+  green: 'green';
+  yellow: 'yellow';
+  red: 'red';
+  magenta: 'magenta';
+  rainbow: 'rainbow';
+}
+
+export default Color;
