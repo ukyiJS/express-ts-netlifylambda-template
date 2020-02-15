60d2956a13cf4996811a81766cbab2612f8b4753
UkYi <ukyi.js@gmail.com>
2020-02-15 20:54:05 +0900

    add httpException

--- src/exceptions/httpException.ts
+++ src/exceptions/httpException.ts
@@ -1,12 +1,16 @@
+import log, { ERROR } from '../utils/log';
+
 class HttpException extends Error {
   public status: number;
-
   public message: string;
 
   constructor(status: number, message: string) {
     super(message);
+
     this.status = status;
     this.message = message;
+
+    log(ERROR)(message);
   }
 }
 
