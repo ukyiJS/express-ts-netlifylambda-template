4f59c9602a491fc64473c015bf527de901a6857b
UkYi <ukyi.js@gmail.com>
2020-02-15 20:54:52 +0900

    add log

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,7 +1,25 @@
 import 'colors';
-import consola from 'consola';
+import consola, { Consola } from 'consola';
+import Color from '../interfaces/color';
 
-export const INFO = (message: string) => consola.info({ message: message.cyan, badge: true });
-export const ERROR = (message: string) => consola.error({ message: message.red, badge: true });
-export const SUCCESS = (message: string) => consola.success({ message: message.green, badge: true });
-export const READY = (message: string) => consola.ready({ message: message.green, badge: true });
+export const LOG = 'log';
+export const INFO = 'info';
+export const WARN = 'warn';
+export const SUCCESS = 'success';
+export const READY = 'ready';
+export const ERROR = 'error';
+
+export default function log(methods: keyof Consola = LOG) {
+  return (message: string, type?: string, colorType?: keyof Color) => {
+    let color: keyof Color = 'white';
+
+    if (methods === INFO) color = 'cyan';
+    if (methods === SUCCESS || methods === READY) color = 'green';
+    if (methods === WARN) color = 'yellow';
+    if (methods === ERROR) color = 'red';
+
+    const option = { message: message[colorType || color], badge: true, type };
+
+    consola[methods](option);
+  };
+}
